1e04e8a5c932eb719d1a14a9a89060cdcba0ee29
thmedeiroslima <97640552+thmedeiroslima@users.noreply.github.com>
2023-01-17 13:16:54 -0300

    done:
    Validate data fields info
    
    create user account
    
    create function login in app
    
    create function recover account

--- app/src/main/java/com/example/tasklist/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/tasklist/ui/auth/LoginFragment.kt
@@ -5,16 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.example.tasklist.R
 import com.example.tasklist.databinding.FragmentLoginBinding
 import com.example.tasklist.databinding.FragmentSplashBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,17 +33,57 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+
         initCliks()
     }
 
     private fun initCliks(){
+
+        binding.btnLogin.setOnClickListener { validateData() }
+
         binding.btnRegister.setOnClickListener{
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
+
         binding.btnRecover.setOnClickListener{
             findNavController().navigate(R.id.action_loginFragment_to_recoveryAccountFragment)
         }
     }
+
+    private fun validateData(){
+        val email = binding.edtEmail.text.toString().trim()
+        val password = binding.edtPassword.text.toString().trim()
+
+        if(email.isNotEmpty()){
+            if(password.isNotEmpty()){
+
+                binding.progressBar.isVisible = true
+
+                loginUser(email, password)
+
+            }else{
+                Toast.makeText(requireContext(), "Informe sua senha..", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            Toast.makeText(requireContext(), "Informe seu email.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun loginUser(email: String, password: String){
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment2)
+                } else {
+                    binding.progressBar.isVisible = false
+                }
+            }
+
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/tasklist/ui/auth/RecoveryAccountFragment.kt
+++ app/src/main/java/com/example/tasklist/ui/auth/RecoveryAccountFragment.kt
@@ -5,15 +5,66 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
 import com.example.tasklist.R
+import com.example.tasklist.databinding.FragmentLoginBinding
+import com.example.tasklist.databinding.FragmentRecoveryAccountBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RecoveryAccountFragment : Fragment() {
 
+    private var _binding: FragmentRecoveryAccountBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recovery_account, container, false)
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecoveryAccountBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+
+        initCliks()
+    }
+    private fun initCliks() {
+        binding.btnSend.setOnClickListener { validateData() }
+    }
+
+    private fun validateData(){
+        val email = binding.edtEmail.text.toString().trim()
+
+        if(email.isNotEmpty()){
+            binding.progressBar.isVisible = true
+
+            recoverAccountUser(email)
+
+        }else{
+            Toast.makeText(requireContext(), "Informe seu email.", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun recoverAccountUser(email: String){
+
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(requireContext(),"O Link de recuperação de senha foi enviado para seu E-mail", Toast.LENGTH_SHORT).show()
+                }
+                binding.progressBar.isVisible = false
+            }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
     }
 
 }
